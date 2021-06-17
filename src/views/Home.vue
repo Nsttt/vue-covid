38fd79e5eb03adb206a96f35bc6e24d19ca86e01
Nestor <elnestinatorHD@gmail.com>
2021-06-17 19:24:04 +0200

    feat: home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,9 @@
     <DataTitle :text="title" :dataDate="dataDate" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <p class="w-24 m-auto">⏳</p>
+  </main>
 </template>
 
 <script>
@@ -13,6 +16,29 @@ export default {
   name: "Home",
   components: {
     DataTitle,
+  },
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: "",
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await axios.get("https://api.covid19api.com/summary");
+      return res.data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
   },
 };
 </script>
