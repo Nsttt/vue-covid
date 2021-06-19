39ad78ba5ee9c11351a3bf30943f7055ad43833f
Nestor <elnestinatorHD@gmail.com>
2021-06-19 13:37:56 +0200

    feat: data title component

---
+++ src/components/DateTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-center">
+    <h2 class="text-3xl font-bold">
+      {{ text }}
+    </h2>
+    <div class="text-2xl mt-4 mb-10">{{ timestamp }}</div>
+  </div>
+</template>
+
+<script>
+import { DateTime } from "luxon";
+
+export default {
+  name: "DataTitle",
+  props: ["text", "dataDate"],
+  computed: {
+    timestamp: function () {
+      return DateTime.fromISO(this.dataDate).toLocaleString(
+        DateTime.DATETIME_FULL
+      );
+    },
+  },
+};
+</script>
